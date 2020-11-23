6e819695e35b1153da6f786a9f4b8fbee773469a
oberon02 <71565479+oberon02@users.noreply.github.com>
2020-11-23 11:00:09 +0300

    Update snake_test.py
    test2

--- snake_test.py
+++ snake_test.py
@@ -1,8 +1,9 @@
 import pygame as pg
 import time
-import random
+import numpy as np
+from random import randint, gauss
 
-pygame.init()
+pg.init()
 
 white = (255, 255, 255)
 yellow = (255, 255, 102)
@@ -11,18 +12,33 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-screen = pg.display.set_mode(SCREEN_SIZE)
+screen_size = [1440, 900]
+
+screen = pg.display.set_mode(screen_size)
 
 
 class Snake1:
-    def __init__(self):
+    def __init__(self, color, coord=None, length=0, width=10):
+        if coord == None:
+            coord = [randint(0, screen_size[0] - self.width), randint(0, screen_size[0] - self.width)]
+        self.coord = coord
+        self.length = length
+        self.width = width
+        self.color = color
+        self.snake_blocks_coord = []
+
         pass
 
     def move(self):
         pass
 
     def draw(self):
+        for coord in self.snake_blocks_coord:
+            pg.draw.rect(screen, self.color, [coord, self.width, self.width])
         pass
+
+    def eat(self):
+        self.length += 1
 
 
 class Snake2:
@@ -43,6 +59,15 @@ class Food:
 class Manager:
 
     def __init__(self):
+        self.food = []
+
+        self.snake = Snake1()
+        self.targets = []
+        self.score_t = ScoreTable()
+        self.n_static_targets = n_static_targets
+        self.n_moving_targets = n_moving_targets
+        self.new_mission()
+
         pass
 
     def new_food(self):
