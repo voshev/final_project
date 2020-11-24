de20272d86439f723a181f3e95d098346c3d0881
oberon02 <71565479+oberon02@users.noreply.github.com>
2020-11-24 16:09:11 +0300

    Update snake_test.py

--- snake_test.py
+++ snake_test.py
@@ -24,13 +24,28 @@ class Snake1:
         self.length = length
         self.width = width
         if coord == None:
-            coord = [randint(0, screen_size[0] - self.width), randint(0, screen_size[0] - self.width), 0]
+            coord = [randint(0, int(screen_size[0]/self.width)-1) * self.width, randint(0, int(screen_size[1]/self.width)-1) * self.width, 'right']
         self.coord = coord
-        self.snake_blocks = []
+        self.snake_blocks = [self.coord]
         pass
 
-    def move(self):
-        pass
+    def move_x(self, dir):
+        if dir == 1:
+            self.snake_blocks.insert(0,
+                [self.snake_blocks[0][0] + self.width, self.snake_blocks[0][1], 'right'])
+        elif dir == -1:
+            self.snake_blocks.insert(0,
+                [self.snake_blocks[0][0] - self.width, self.snake_blocks[0][1], 'left'])
+        self.snake_blocks.pop(self.length)
+
+    def move_y(self, dir):
+        if dir == 1:
+            self.snake_blocks.insert(0,
+                [self.snake_blocks[0][0], self.snake_blocks[0][1] + self.width, 'down'])
+        elif dir == -1:
+            self.snake_blocks.insert(0,
+                [self.snake_blocks[0][0], self.snake_blocks[0][1] - self.width, 'up'])
+        self.snake_blocks.pop(self.length)
 
     def draw(self):
         for coord in self.snake_blocks:
@@ -48,13 +63,29 @@ class Snake2:
         self.length = length
         self.width = width
         if coord == None:
-            coord = [randint(0, screen_size[0] - self.width), randint(0, screen_size[0] - self.width), 0]
+            coord = [randint(0, int(screen_size[0]/self.width)-1) * self.width, randint(0, int(screen_size[1]/self.width)-1) * self.width, 'right']
         self.coord = coord
         self.snake_blocks = []
         pass
 
-    def move(self):
-        pass
+    def move_x(self, dir):
+        if dir == 1:
+            self.snake_blocks.insert(0,
+                [self.snake_blocks[0][0] + self.width, self.snake_blocks[0][1], 'right'])
+        elif dir == -1:
+            self.snake_blocks.insert(0,
+                [self.snake_blocks[0][0] - self.width, self.snake_blocks[0][1], 'left'])
+
+        self.snake_blocks.pop(self.length)
+
+    def move_y(self, dir):
+        if dir == 1:
+            self.snake_blocks.insert(0,
+                [self.snake_blocks[0][0], self.snake_blocks[0][1] + self.width, 'down'])
+        elif dir == -1:
+            self.snake_blocks.insert(0,
+                [self.snake_blocks[0][0], self.snake_blocks[0][1] - self.width, 'up'])
+        self.snake_blocks.pop(self.length)
 
     def draw(self):
         for coord in self.snake_blocks:
@@ -66,9 +97,14 @@ class Snake2:
 
 
 class Food:
-    def __init__(self, coord):
+    def __init__(self, color, coord = None, width=20):
+        self.color = color
+        self.width = width
+        if coord == None:
+            coord = [randint(0, int(screen_size[0]/self.width)-1) * self.width, randint(0, int(screen_size[1]/self.width)-1) * self.width]
         self.coord = coord
-
+    def draw(self):
+        pg.draw.rect(screen, self.color, [self.coord[0], self.coord[1], self.width, self.width])
     pass
 
 
@@ -83,7 +119,7 @@ class Manager:
         pass
 
     def new_food(self):
-
+        self.food.insert(0,Food(green))
         pass
 
     def process(self, events, screen):
@@ -101,10 +137,36 @@ class Manager:
 
     def handle_events(self, events):
         done = False
+        for event in events:
+            if event.type == pg.QUIT:
+                done = True
+
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    self.snake1.move_y(-1)
+                elif event.key == pg.K_DOWN:
+                    self.snake1.move_y(1)
+                elif event.key == pg.K_RIGHT:
+                    self.snake1.move_x(1)
+                elif event.key == pg.K_LEFT:
+                    self.snake1.move_x(-1)
+                elif event.key == pg.K_UP:
+                    self.snake2.move_y(-1)
+                elif event.key == pg.K_DOWN:
+                    self.snake2.move_y(1)
+                elif event.key == pg.K_RIGHT:
+                    self.snake2.move_x(1)
+                elif event.key == pg.K_LEFT:
+                    self.snake2.move_x(-1)
 
         return done
 
-    def draw(self, screen):
+    def draw(self,screen):
+        for food in self.food:
+            food.draw()
+        for snake in self.snakes:
+            snake.draw()
+
         pass
 
     def move(self):
@@ -115,7 +177,7 @@ class Manager:
 
     def block_direction(self):
         for snake in self.snakes:
-            n = len(snake.snake_blocks)
+            n = snake.length
             for i in range(1, n - 2):
                 if (snake.snake_blocks[i - 1][0] - snake.snake_blocks[i + 1][0] == 0):
                     snake.snake_blocks[i][2] = 'vertical'
@@ -183,3 +245,4 @@ while not done:
     pg.display.flip()
 
 pg.quit()
+
