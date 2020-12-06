8d542ea6eb492ad2ef08db49641ce0ca9027efca
oberon02 <71565479+oberon02@users.noreply.github.com>
2020-12-06 23:00:36 +0300

    Update snake_test.py

--- snake_test.py
+++ snake_test.py
@@ -2,8 +2,26 @@ import pygame as pg
 import time
 import numpy as np
 from random import randint, gauss
+import os
 
 pg.init()
+
+horizontal = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+vertical = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+up_right = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+right_down = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+down_left = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+left_up = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+
+head_right = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+head_down = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+head_left = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+head_up = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+
+tail_right = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+tail_down = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+tail_left = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
+tail_up = pg.image.load(os.path.join("images", "Snake Images", "Snake_main.jpg"))
 
 white = (255, 255, 255)
 yellow = (255, 255, 102)
@@ -19,16 +37,17 @@ screen = pg.display.set_mode(screen_size)
 
 class Snake1:
     # предпологается, что snake1 на стрелочках snake2 на awsd в остальном они одинаковы
-    def __init__(self, color, coord=None, length=1, width=20, move_direction=0):
+    def __init__(self, color, lives=1, start_coord=None, length=1, width=20, move_direction=0):
+        self.lives = lives
         self.move_direction = move_direction
         self.color = color
         self.length = length
         self.width = width
-        if coord == None:
-            coord = [randint(0, int(screen_size[0] / self.width) - 1) * self.width,
+        if start_coord == None:
+            start_coord = [randint(0, int(screen_size[0] / self.width) - 1) * self.width,
                      randint(0, int(screen_size[1] / self.width) - 1) * self.width, 'right']
-        self.coord = coord
-        self.snake_blocks = [self.coord]
+        self.start_coord = start_coord
+        self.snake_blocks = [self.start_coord]
         pass
 
     def move_x(self, dir):
@@ -52,9 +71,59 @@ class Snake1:
             self.snake_blocks.pop(self.length)
 
     def draw(self):
-        for coord in self.snake_blocks:
-            pg.draw.rect(screen, self.color, [coord[0], coord[1], self.width, self.width])
-        pass
+        for block in self.snake_blocks:
+
+            # рисует хвост
+            if self.length >= 2:
+                if block == self.snake_blocks[self.length-1]:
+                    if block[2] == 'up':
+                        block_coord = [block[0], block[1]]
+                        screen.blit(tail_up, tail_up.get_rect(topleft=block_coord))
+                    elif block[2] == 'right':
+                        block_coord = [block[0], block[1]]
+                        screen.blit(tail_right, tail_right.get_rect(topleft=block_coord))
+                    elif block[2] == 'down':
+                        block_coord = [block[0], block[1]]
+                        screen.blit(tail_down, tail_down.get_rect(topleft=block_coord))
+                    elif block[2] == 'left':
+                        block_coord = [block[0], block[1]]
+                        screen.blit(tail_left, tail_left.get_rect(topleft=block_coord))
+
+            # рисует голову
+            elif block == self.snake_blocks[0]:
+                if block[2] == 'right':
+                    block_coord = [block[0], block[1]]
+                    screen.blit(head_right, head_right.get_rect(topleft=block_coord))
+                elif block[2] == 'down':
+                    block_coord = [block[0], block[1]]
+                    screen.blit(head_down, head_down.get_rect(topleft=block_coord))
+                elif block[2] == 'left':
+                    block_coord = [block[0], block[1]]
+                    screen.blit(head_left, head_left.get_rect(topleft=block_coord))
+                elif block[2] == 'up':
+                    block_coord = [block[0], block[1]]
+                    screen.blit(head_up, head_up.get_rect(topleft=block_coord))
+
+            # рисует туловищу
+            if block[2] == 'horizontal':
+                block_coord = [block[0], block[1]]
+                screen.blit(horizontal, horizontal.get_rect(topleft=block_coord))
+            elif block[2] == 'vertical':
+                block_coord = [block[0], block[1]]
+                screen.blit(vertical, vertical.get_rect(topleft=block_coord))
+            elif block[2] == 'up_right':
+                block_coord = [block[0], block[1]]
+                screen.blit(up_right, up_right.get_rect(topleft=block_coord))
+            elif block[2] == 'right_down':
+                block_coord = [block[0], block[1]]
+                screen.blit(right_down, right_down.get_rect(topleft=block_coord))
+            elif block[2] == 'down_left':
+                block_coord = [block[0], block[1]]
+                screen.blit(down_left, down_left.get_rect(topleft=block_coord))
+            elif block[2] == 'left_up':
+                block_coord = [block[0], block[1]]
+                screen.blit(left_up, left_up.get_rect(topleft=block_coord))
+
 
     def grow(self):
         self.length += 1
@@ -62,7 +131,8 @@ class Snake1:
 
 class Snake2:
     # предпологается, что snake1 на стрелочках snake2 на awsd в остальном они одинаковы
-    def __init__(self, color, coord=None, length=1, width=20, move_direction=0):
+    def __init__(self, color, lives=1, coord=None, length=1, width=20, move_direction=0):
+        self.lives = lives
         self.move_direction = move_direction
         self.color = color
         self.length = length
@@ -137,16 +207,20 @@ class Manager:
 
         self.walls_update()
 
-        self.collide()
-
         self.block_direction()
 
         self.draw()
+
+        self.collide()
+
 
         return done
 
     def handle_events(self, events):
         done = False
+        for snake in self.snakes:
+            if snake.lives == 0:
+                done = True
         for event in events:
             if event.type == pg.QUIT:
                 done = True
@@ -176,7 +250,6 @@ class Manager:
             food.draw()
         for snake in self.snakes:
             snake.draw()
-        pass
 
     def move(self):
         for snake in self.snakes:
@@ -213,12 +286,12 @@ class Manager:
                 for wall in self.walls:
                     if [snake.snake_blocks[i][0], snake.snake_blocks[i][1]] == wall:
                         snake.move_direction = 0
-
+                        snake.lives -= 1
 
     def block_direction(self):
         for snake in self.snakes:
             n = snake.length
-            for i in range(1, n - 2):
+            for i in range(1, n-1):
                 if (snake.snake_blocks[i - 1][0] - snake.snake_blocks[i + 1][0] == 0):
                     snake.snake_blocks[i][2] = 'vertical'
                 elif (snake.snake_blocks[i - 1][1] - snake.snake_blocks[i + 1][1] == 0):
@@ -244,9 +317,7 @@ class Manager:
                       snake.snake_blocks[i][0] < 0):
                     snake.snake_blocks[i][2] = 'left-up'
 
-            snake.snake_blocks[0][2] = snake.move_direction
-
-            if n > 30:
+            if n >= 2:
                 if (snake.snake_blocks[n - 1][0] - snake.snake_blocks[n - 2][0] == 0 and
                         snake.snake_blocks[n - 1][1] - snake.snake_blocks[n - 2][1] < 0):
                     snake.snake_blocks[n - 1][2] = 'up'
@@ -258,7 +329,7 @@ class Manager:
                     snake.snake_blocks[n - 1][2] = 'left'
                 elif (snake.snake_blocks[n - 1][0] - snake.snake_blocks[n - 2][0] > 0 and
                       snake.snake_blocks[n - 1][1] - snake.snake_blocks[n - 2][1] == 0):
-                    snake.snake_blocks[n - 1][2] = 'left'
+                    snake.snake_blocks[n - 1][2] = 'right'
 
 
 done = False
@@ -268,7 +339,7 @@ clock = pg.time.Clock()
 mgr = Manager()
 
 while not done:
-    clock.tick(20)
+    clock.tick(4)
     screen.fill(black)
 
     done = mgr.process(pg.event.get(), screen)
