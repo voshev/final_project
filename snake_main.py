cb31ea1156cf34b75d33bbdd0a30bbff30b6aef1
voshev <bobylev.aiu@phystech.edu>
2020-12-07 10:45:01 +0300

    added new buttons but list is always visible

--- snake_main.py
+++ snake_main.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.draw import *
 import os
 
 FPS = 8
@@ -21,7 +20,7 @@ def button(message, x, y, button_width, button_height, active_color, font_color,
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
-    if x + button_width > mouse[0] > x and y + button_height > mouse[1] > y:
+    if (x < mouse[0] < x + button_width) and (y < mouse[1] < y + button_height):
         pygame.draw.rect(game_window, active_color, (x, y, button_width, button_height))
         if click[0] == 1 and action is not None:
             action()
@@ -38,12 +37,17 @@ def game_loop():
     pass
 
 
+def list_stop():
+    pass
+
+
 def list_of_levels():
-    pass
+    button('2', 1110, 395, 80, 50, BUDDHA_GOLD, SKOBELOFF, list_stop())
+    button('3', 1195, 395, 80, 50, BUDDHA_GOLD, SKOBELOFF, list_stop())
 
 
-def list_of_answers():
-    pass
+def list_of_answers(x, y):
+    button('YES', x, y, 80, 50, BUDDHA_GOLD, SKOBELOFF, list_stop())
 
 
 def main():
@@ -54,8 +58,8 @@ def main():
 
     button('SNAKE!', 600, 750, 250, 50, SKOBELOFF, BUDDHA_GOLD, game_loop())
     button('1', 1025, 395, 80, 50, BUDDHA_GOLD, SKOBELOFF,  list_of_levels())
-    button('NO', 1025, 494, 80, 50, BUDDHA_GOLD, SKOBELOFF, list_of_answers())
-    button('NO', 1025, 593, 80, 50, BUDDHA_GOLD, SKOBELOFF, list_of_answers())
+    button('NO', 1025, 494, 80, 50, BUDDHA_GOLD, SKOBELOFF, list_of_answers(1110, 494))
+    button('NO', 1025, 593, 80, 50, BUDDHA_GOLD, SKOBELOFF, list_of_answers(1110, 593))
 
     clock = pygame.time.Clock()
     finished = False
